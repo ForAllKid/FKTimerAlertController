39e4b7547d94bd6263ca08ab8e751e69b416e049
ForKid <910784384@qq.com>
2017-07-13 11:29:35 +0800

    first blood

---
+++ FKTimerAlertController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FKTimerAlertController
+//
+//  Created by 周宏辉 on 2017/7/13.
+//  Copyright © 2017年 ForKid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
